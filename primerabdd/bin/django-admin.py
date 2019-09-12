524ad94cd937e587668ff8a68e4cd09e003bb8da
Agustin <agus.luques@hotmail.com>
2019-09-12 13:07:23 -0300

    agrego virtualenv y postgresql

---
+++ primerabdd/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Agus/Facultad/2019-2do/TDP3/ONG-Management/primerabdd/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
