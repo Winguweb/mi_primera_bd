c5ddf527ea801998be220785f0ea3d6e1a877454
Agustin <agus.luques@hotmail.com>
2019-10-09 16:22:33 -0300

    preparo deploy a heroku

--- primerabdd/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/Agus/Facultad/2019-2do/TDP3/ONG-Management/primerabdd/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- primerabdd/primerabdd/primerabdd/settings.py
+++ primerabdd/primerabdd/primerabdd/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,3 +136,9 @@ STATICFILES_DIRS = [
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
+
