94dfd62f25c0a63fc10ba2fa04e92d0b23e33ef2
jmarshall-navent <jmarshall@navent>
2019-10-20 16:15:43 -0300

    Vista de contactos de una cuenta

--- primerabdd/primerabdd/crm/urls.py
+++ primerabdd/primerabdd/crm/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('cuentas/', views.CuentasLista.as_view(), name='ver_cuentas'),
     path('contactos/', views.ContactosPorNivel.as_view(), name='contactos'),
     path('cuentas/<int:pk>/', views.CuentasDetalles.as_view(), name='cuentas_detalles'),
+    path('cuentas/<int:pk>/contactos', views.CuentasContactos.as_view(), name='cuentas_contactos'),
     path('delete/<int:pk>', views.ContactoEliminar.as_view(), name='eliminar_contacto'),
     path('create/', views.ContactoCrear.as_view(), name='crear_contacto'),
     path('edit/<int:pk>', views.ContactoEditar.as_view(), name='editar_contacto'),
