0ab42d6c9e223f1e2aaabe23204943828d946061
jmarshall-navent <jmarshall@navent>
2019-10-20 21:13:59 -0300

    Arreglo query de contacto dentro de una cuenta

--- primerabdd/primerabdd/crm/urls.py
+++ primerabdd/primerabdd/crm/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path('cuentas/', views.CuentasLista.as_view(), name='ver_cuentas'),
     path('contactos/', views.ContactosPorNivel.as_view(), name='contactos'),
-    path('cuentas/<int:pk>/', views.CuentasDetalles.as_view(), name='cuentas_detalles'),
-    path('cuentas/<int:pk>/contactos', views.CuentasContactos.as_view(), name='cuentas_contactos'),
+    path('cuentas/<int:pk>/contactos/', views.CuentasContactos.as_view(), name='cuentas_contactos'),
+    path('cuentas/<int:pk>', views.CuentasDetalles.as_view(), name='cuentas_detalles'),
     path('delete/<int:pk>', views.ContactoEliminar.as_view(), name='eliminar_contacto'),
     path('create/', views.ContactoCrear.as_view(), name='crear_contacto'),
     path('edit/<int:pk>', views.ContactoEditar.as_view(), name='editar_contacto'),
