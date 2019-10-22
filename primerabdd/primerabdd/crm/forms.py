357bc0546c325659ba09ce38c1ac86cf1e359d1f
Agustin <agus.luques@hotmail.com>
2019-10-22 14:37:32 -0300

    cambio modelo voluntario

--- primerabdd/primerabdd/crm/forms.py
+++ primerabdd/primerabdd/crm/forms.py
@@ -1,7 +1,7 @@
-from .models import Organizacion, Cuenta, Contacto, Voluntario, Donante
+from .models import Organizacion, Cuenta, Contacto, Voluntario
 from django import forms
 from django.forms import ModelForm, CheckboxInput
-from django.forms.models import inlineformset_factory
+
 
 from django_select2.forms import Select2Widget
 
@@ -9,30 +9,21 @@ class DateInput(forms.DateInput):
     input_type = 'date'
 
 class ContactoCrearForm(ModelForm):
-    donanteCheckBox = forms.BooleanField(required=False, label='Donante', widget=CheckboxInput(attrs={'id': 'checkDonante',}))
-    voluntarioCheckBox = forms.BooleanField(required=False, label='Voluntario', widget=CheckboxInput(attrs={'id': 'checkVoluntario',}))
-
     class Meta:
         model = Contacto
         fields = '__all__'
 
 
         widgets = {
-            'cuenta': Select2Widget(attrs={'data-placeholder':"Crear cuenta nueva"}),
-            'fecha_de_nacimiento': DateInput()
+            'cuenta': Select2Widget(attrs={'data-placeholder':"Vacío para crear cuenta nueva"}),
+            'origen': Select2Widget(attrs={'data-placeholder':""}),
+            'categoria': Select2Widget(attrs={'data-placeholder':""}),
+            'sexo': Select2Widget(attrs={'data-placeholder':""}),
+            'fecha_de_nacimiento': DateInput(),
+            'estado': Select2Widget(attrs={'data-placeholder':"Estado"}),
+            'turno': Select2Widget(attrs={'data-placeholder':"Turno"}),
+            'habilidades': Select2Widget(attrs={'data-placeholder':"Habilidades"}),
         }
 
     def __init__(self, *args, **kwargs):
         super(ContactoCrearForm, self).__init__(*args, **kwargs)
-        self.fields['cuenta'].empty_label = "Crear cuenta automáticamente"
-        self.fields['cuenta'].widget.choices = self.fields['cuenta'].choices
-        
-class DonanteCrearForm(ModelForm):
-    class Meta:
-        model = Donante
-        exclude=['contacto']
-
-class VoluntarioCrearForm(ModelForm):
-    class Meta:
-        model = Voluntario
-        exclude=['contacto']
